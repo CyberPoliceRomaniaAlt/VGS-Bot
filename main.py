1dbcb95c38ea3b2fa1e0c949a996e53064379a84
CyberPoliceRomania <hackerclkidd11@gmail.com>
2025-12-24 14:24:59 +0200

    Set up Discord bot with command prefix and intents

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import os
+import discord
+from discord.ext import commands
 
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!vgs", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"[VGS] Online ca {bot.user}")
+
+TOKEN = os.getenv("TOKEN")
+
+if not TOKEN:
+    raise RuntimeError("TOKEN nu este setat!")
+
+bot.run(TOKEN)
